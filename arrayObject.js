a8122bea92118965b356e1f9d4c8ed972643b135
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-06-30 06:34:30 +0000

    Array and Object

---
+++ arrayObject.js
@@ -0,0 +1,20 @@
+const techMorphsTasks = [
+  {
+    title: "Design AI Sahayak frontend dashboard",
+    status: "completed"
+  },
+  {
+    title: "Set up Node.js backend server",
+    status: "pending"
+  },
+  {
+    title: "Test rockfall alert system logic",
+    status: "in-progress"
+  }
+];
+
+console.log("--- Project Dashboard ---");
+
+console.log("Task 1:", techMorphsTasks[0].title);
+
+console.log("Task 2 Status:", techMorphsTasks[1].status);
