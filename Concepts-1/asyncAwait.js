a6660aad96e66b87358ae8748f95f259bc00694b
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-07-07 08:43:32 +0000

    Async Await

---
+++ Concepts-1/asyncAwait.js
@@ -0,0 +1,12 @@
+async function getUser() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+        const data = await response.json();
+
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getUser();
