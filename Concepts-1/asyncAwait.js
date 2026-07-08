390b51d6dac37a5434a4ae0fe66e4fd28f350f6f
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-07-08 15:29:55 +0000

    Change

--- Concepts-1/asyncAwait.js
+++ Concepts-1/asyncAwait.js
@@ -1,4 +1,4 @@
-async function getUser() {
+async function getUserData() {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
         const data = await response.json();
@@ -9,4 +9,4 @@ async function getUser() {
     }
 }
 
-getUser();
+getUserData();
