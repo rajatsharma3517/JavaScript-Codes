57c7ee451aaaa045a3e71e78e03cce468f84beee
Rajat Sharma <149601642+rajatsharma3517@users.noreply.github.com>
2026-07-13 15:54:58 +0000

    Inheritance concept

---
+++ Concepts-1/inheritance.js
@@ -0,0 +1,14 @@
+class Animal {
+    speak() {
+        console.log("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    speak() {
+        console.log("Dog barks");
+    }
+}
+
+const dog = new Dog();
+dog.speak();
